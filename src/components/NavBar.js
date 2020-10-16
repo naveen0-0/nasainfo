97b776ec09e2ac6e189e9e53366eafa581296aeb
NAVEEN <Naveen>
2020-10-16 15:27:06 +0530

    ui updates to homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
     return (
         <div>
-            Navbar
+            <div className="navtitle">SpaceStuff</div>
         </div>
     )
 }
--- src/components/PictureOfTheDay.js
+++ src/components/PictureOfTheDay.js
@@ -1,25 +1,40 @@
 import React,{ useState, useEffect } from 'react';
 import Axios from 'axios'; 
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
 export default function PictureOfTheDay() {
     let [ pictureOfTheDay, setPictureOfTheDay ] = useState({});
+    let [ imageOrNot, setImageOrNot ] = useState(false)
+    let [ available, setAvailable ] = useState(false);
 
     useEffect(()=>{
         async function getPictureOfTheDay(){
             let { data } = await Axios.get(`https://api.nasa.gov/planetary/apod?api_key=IWzif6oTa6550M09AqRTw8kK3gumG9VfjqR20dnL`);
-            setPictureOfTheDay(data)
+            setPictureOfTheDay(data);
+            setAvailable(true)
+            if(pictureOfTheDay.media_type === "image"){
+                setImageOrNot(true)
+            }
         }
         getPictureOfTheDay()
-    },[])
+    },[pictureOfTheDay.media_type])
+
+    if(!available){
+        return <div className="loading">Loading...</div>
+    }
 
     return (
         <div className="pictureOfTheDayContainer">
-            <div className="picture">Today's Picture</div>
+            <div className="picture">Today's {imageOrNot? "Picture" : "Video"}</div>
             <div className="title">{pictureOfTheDay.title}</div>
-            <img src={pictureOfTheDay.hdurl} alt="PictureOfTheDay" className="pictureOfTheDay"/>
-            <div className="copyright">{pictureOfTheDay.copyright}</div>
+            {imageOrNot? (
+                <img src={pictureOfTheDay.hdurl} alt="PictureOfTheDay" className="pictureOfTheDay"/>
+            ):(
+                <div className="video">
+                    <a href={pictureOfTheDay.url} target="_blank" rel="noopener noreferrer" className="actualVideo">Click it to see the video</a>
+                </div>
+            )}
+
+            <div className="copyright">Copyright by {pictureOfTheDay.copyright}</div>
             <div className="explanation">
                 {pictureOfTheDay.explanation}
             </div>
